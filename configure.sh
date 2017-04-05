31c1f862507c423e79c8789ed8608b68e84a9628
guher.bozdogan <guher.bozdogan.wrk@gmail.com>
2017-04-05 20:40:54 +0300

    add/guherbozdogan: *adding auto generated mesos proto & pb files from mesos 1.2.0 version

--- configure.sh
+++ configure.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 ##note/guherbozdogan: 
-#add  dependency checking of git version (> 1.7) and 
+#add  dependency checking of git version (>= 1.7) and 
 #add dependency checking of installation of github.com/gogo/protobuf projects
 
 
 #downloads mesos proto folder 
 function downloadGitFolder {
-cd mesos&& git init && git config core.sparsecheckout true &&  echo 'include/mesos/v1' > .git/info/sparse-checkout && git remote add -f origin git://github.com/apache/mesos.git &&  git pull origin master && mv include/mesos/v1 v1 && rm -rf include/mesos
+cd src/client/pb/mesos && git init && git config core.sparsecheckout true &&  echo 'include/mesos/v1' > .git/info/sparse-checkout && git remote add -f origin git://github.com/apache/mesos.git &&  git pull origin master && mv include/mesos/v1 ./v1 && rm -rf include/mesos
 }
 
 
 #generates the protobuf go files 
 function callProtoBuffGen {
-cd mesos && find ./v1/.  -type f ! -name '*.proto' -exec rm {} \; && @find . -type f -name '*.proto' -exec protoc   --proto_path=${GOPATH}/src:${GOPATH}/src/github.com/gogo/protobuf/protobuf:. --gogo_out=.   {} \;
+cd  src/client/pb/mesos && find ./v1/.  -type f ! -name '*.proto' -exec rm {} \; && @find . -type f -name '*.proto' -exec protoc   --proto_path=${GOPATH}/src:${GOPATH}/src/github.com/gogo/protobuf/protobuf:. --gogo_out=.   {} \;
 }
 
 ##note/guherbozdogan:
