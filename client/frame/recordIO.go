cbd46195bf6b3fce9f099cb4974578df81ab90da
guher.bozdogan <guher.bozdogan.wrk@gmail.com>
2017-08-10 00:54:31 +0300

    add/guherbozdogan: * working  ut case defs

--- client/frame/recordIO.go
+++ client/frame/recordIO.go
@@ -1,177 +1,181 @@
 package frame
-	
-import  ( 
-    "sync/atomic"
-    "sync"
-    binary "encoding/binary"
-    "bufio"
-    "context"
-    "io"
-    "bytes"
-    "errors"
-    )
 
-type RecordIO  struct {
-        
- }
+import (
+	"bufio"
+	//	"bytes"
+	"context"
+	//binary "encoding/binary"
+	"errors"
+	"io"
+	"sync"
+	"sync/atomic"
 
-func NewRecordIO()  FrameIO {
-    return &RecordIO{}
+	//	"bytes"
+	"fmt"
+	"strconv"
+)
+
+type RecordIO struct {
 }
-type Bytes  []byte
+
+func NewRecordIO() FrameIO {
+	return &RecordIO{}
+}
+
+type Bytes []byte
 
 //helper functionto read a line with bufio
-func Readln(r  *bufio.Reader) (Bytes, error) {
-    var (
-        isPrefix bool = true
-        err error = nil
-        line, ln []byte
-    )
-    for isPrefix && err == nil {
-        line, isPrefix, err = r.ReadLine()
-        ln = append(ln, line...)
-    }
-    return ln,err
+func Readln(r *bufio.Reader) (Bytes, error) {
+	var (
+		isPrefix bool  = true
+		err      error = nil
+		line, ln []byte
+	)
+	for isPrefix && err == nil {
+		line, isPrefix, err = r.ReadLine()
+		ln = append(ln, line...)
+	}
+	return ln, err
 }
 
 //helper function to check whether a flag is set or not in atomic sense
-func isAtomicValSet( v *uint32 ) bool {
-    opsFinal := atomic.LoadUint32(v)
-    if opsFinal > 0 {
-        return true;
-    }    else {
-        return false;
-    }
+func isAtomicValSet(v *uint32) bool {
+	opsFinal := atomic.LoadUint32(v)
+	if opsFinal > 0 {
+		return true
+	} else {
+		return false
+	}
 }
-    
 
 //interface function for Read
-func (rc RecordIO )  Read(ctx context.Context, reader io.ReadCloser, f FrameReadFunc, erf ErrorFunc)   {
+func (rc RecordIO) Read(ctx context.Context, reader io.ReadCloser, f FrameReadFunc, erf ErrorFunc) {
 
-    
-    //flag to be set when context is done
-    var icd  uint32 =0;
-    
-    errc:=make(chan error) //channel for sending error out
-    
-    //initialize the bufio
-    r:=bufio.NewReader(reader)
-    
-    //to be able to wait for the graceful exit of below function
-    var wg sync.WaitGroup
-    wg.Add(1)
-    //note: check how channels receive arrays, if they receive as copies, is it possible to send a channel an address of bytes from a local variable? if so, use the pointer version instead of copy by value
-    
-    // go function 
-    //go func (r bufio.Reader,  rfc chan bytes[], erc chan error, ops * uint32,)   {
-    go func ()   {
-        //graceful exit
-        defer wg.Done();
-        
-        //check if context is already cancelled/done, if so, return
-        fcc:=func  () bool    {
-            opsFinal := atomic.LoadUint32(&icd)
-            if opsFinal > 0 {
-                return true;
-            }  else {
-                return false;
-            }   
-        }
-        
-        
-        for ! fcc()  {
-            b, err := Readln(r)
-      
-         
-            //check if context is already cancelled/done, if so, return
-            if fcc() {    
-                return;
-            }
-            if err!= nil {
-                errc <- err  
-                   return;
-            }     
-         
-            buf := bytes.NewBuffer(b) // b is []byte
-            s, err := binary.ReadVarint(buf)
-            if err!= nil {
-                //note/add: convert the error that byte is not read
-                errc <- err         
-                return;
-            }
-        
-            //add some check constraints here to the read buffer size!!!!!!!!!!!  might be illegitimate! 
-         
-            //check for panics of memory alloc later
-         
-            trb := make([] byte, s,s)
-            trbr:= trb[:]
-            l:= int64(0) //length of read bytes
-            for !fcc()  {
-                    ni , err  := r.Read(trbr)
-                    n:=int64(ni)
-                     l+=n
-                    //check if context is already cancelled/done, if so, return
-                    if fcc() {    
-                        return;
-                    }
-             
-                //read entire data
-                // add check if received n is smaller and or n is larger but eof returned
-                if err==io.EOF {
-                    if l<s {
-                     
-                        errc<-errors.New("channel closed before receiving frame")
-                         return;
-                    } else {
-                        trbTmp := make([]byte, s,s)
-                        copy(trbTmp,trb)
-                        f(ctx,Frame(trbTmp),s)
-                    
-                        errc <- errors.New("channel closed")
-                        return;  
-                     }
-                   
-                }    else if l == s {  //if all entire frame read            
-                    trbTmp := make([]byte, s,s)
-                    copy(trbTmp,trb)
-                    f(ctx,Frame(trbTmp),s)
-                     break;
-                    
-                } else {                //if entire frame is not read yet, continue reading in next loop
-                    trbr=trb[l:]
-                }
-            }
-        }
-    }()
-    
-    done := ctx.Done()
-   
-         
-     select {
-     case  <-done:
-         
-         atomic.AddUint32(&icd, 1)
-         //wait graceful close
-         wg.Wait();
-         erf(ctx, ctx.Err())
-         close(errc)
-         return;
-         
-         case    e := <-errc:
-         
-         wg.Wait();
-         erf(ctx,e);
-         close(errc);
-         
-         return 
-     }
-} 
+	//flag to be set when context is done
+	var icd uint32 = 0
 
-func (*RecordIO )  Write(c context.Context,   writer io.WriteCloser, f  FrameWritten, erf ErrorFunc)   {
-   
-    //not need to implement right now:)
-    //to be added later 
-      return 
-        
- }
+	errc := make(chan error) //channel for sending error out
+
+	//initialize the bufio
+	r := bufio.NewReader(reader)
+
+	//to be able to wait for the graceful exit of below function
+	var wg sync.WaitGroup
+	wg.Add(1)
+	//note: check how channels receive arrays, if they receive as copies, is it possible to send a channel an address of bytes from a local variable? if so, use the pointer version instead of copy by value
+
+	// go function
+	//go func (r bufio.Reader,  rfc chan bytes[], erc chan error, ops * uint32,)   {
+	go func() {
+		//graceful exit
+		defer wg.Done()
+
+		//check if context is already cancelled/done, if so, return
+		fcc := func() bool {
+			opsFinal := atomic.LoadUint32(&icd)
+			if opsFinal > 0 {
+				return true
+			} else {
+				return false
+			}
+		}
+
+		for !fcc() {
+			b, err := Readln(r)
+
+			b = append(b, byte(' '))
+			fmt.Print("%s:", b)
+
+			//check if context is already cancelled/done, if so, return
+			if fcc() {
+				return
+			}
+			if err != nil {
+				errc <- err
+				return
+			}
+
+			//buf := bytes.NewBuffer(b) // b is []byte
+			si, err := strconv.Atoi(string(b[:]))
+			s := int64(si)
+			//binary.ReadVarint(buf)
+			if err != nil {
+				//note/add: convert the error that byte is not read
+				errc <- err
+				return
+			}
+
+			//add some check constraints here to the read buffer size!!!!!!!!!!!  might be illegitimate!
+
+			//check for panics of memory alloc later
+
+			trb := make([]byte, s, s)
+			trbr := trb[:]
+			l := int64(0) //length of read bytes
+			for !fcc() {
+				ni, err := r.Read(trbr)
+				n := int64(ni)
+				l += n
+				//check if context is already cancelled/done, if so, return
+				if fcc() {
+					return
+				}
+
+				//read entire data
+				// add check if received n is smaller and or n is larger but eof returned
+				if err == io.EOF {
+					if l < s {
+
+						errc <- errors.New("channel closed before receiving frame")
+						return
+					} else {
+						trbTmp := make([]byte, s, s)
+						copy(trbTmp, trb)
+						f(ctx, Frame(trbTmp), s)
+
+						errc <- errors.New("channel closed")
+						return
+					}
+
+				} else if l == s { //if all entire frame read
+					trbTmp := make([]byte, s, s)
+					copy(trbTmp, trb)
+					f(ctx, Frame(trbTmp), s)
+					break
+
+				} else { //if entire frame is not read yet, continue reading in next loop
+					trbr = trb[l:]
+				}
+			}
+		}
+	}()
+
+	done := ctx.Done()
+
+	select {
+	case <-done:
+
+		atomic.AddUint32(&icd, 1)
+		//wait graceful close
+		wg.Wait()
+		erf(ctx, ctx.Err())
+		close(errc)
+		return
+
+	case e := <-errc:
+
+		wg.Wait()
+		erf(ctx, e)
+		close(errc)
+
+		return
+	}
+}
+
+func (*RecordIO) Write(c context.Context, writer io.WriteCloser, f FrameWritten, erf ErrorFunc) {
+
+	//not need to implement right now:)
+	//to be added later
+	return
+
+}
--- client/frame_test/recordIO_test.go
+++ client/frame_test/recordIO_test.go
@@ -5,8 +5,55 @@ import (
 
 	. "github.com/onsi/ginkgo"
 	//. "github.com/onsi/gomega"
+	"context"
+	//	"fmt"
+	"bytes"
+	"github.com/guherbozdogan/mesos-go-http-client/client/frame"
+	"github.com/onsi/gomega"
+	//"os"
+	"io"
 )
+
+type ClosingBuffer struct {
+	*bytes.Buffer
+}
+
+func (cb *ClosingBuffer) Close() error {
+	//and the error is initialized to no-error
+	return nil
+}
 
 var _ = Describe("RecordIO", func() {
 
+	//rp, rw := io.Pipe()
+	var inputLst = [][]byte{
+		[]byte("120\n{\"type\": \"SUBSCRIBED\",\"subscribed\": {\"framework_id\": {\"value\":\"12220-3440-12532-2345\"},\"heartbeat_interval_seconds\":15.0}"),
+		[]byte(`124\n`)}
+	Describe("Testing Read func", func() {
+		Context("with correct param ", func() {
+
+			recChan := make(chan string, 10)
+			var frameReadFunc = func(c context.Context, f frame.Frame, i int64) context.Context {
+				recChan <- string(f)
+				return c
+			}
+			var errFunc = func(c context.Context, i interface{}) context.Context {
+
+				return c
+			}
+
+			var recIO = frame.NewRecordIO()
+			var tmpRC = &ClosingBuffer{bytes.NewBuffer(inputLst[0])}
+			var rc io.ReadCloser
+			rc = tmpRC
+			recIO.Read(context.Background(), rc, frameReadFunc, errFunc)
+
+			It("should read ", func() {
+				//				buf.Write(inputLst[0])
+
+				gomega.Eventually(recChan).Should(gomega.Receive())
+
+			})
+		})
+	})
 })
